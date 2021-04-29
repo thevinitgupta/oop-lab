cebbd95236e964f30ffc895398fa3841937cd06e
Vinit Gupta <vinitman35@gmail.com>
2021-04-29 22:54:27 +0530

    made some changes in app.java file

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 public class App {
+
+    App(){
+        System.out.println(this);
+    }
     public static void main(String[] args) throws Exception {
+        App app1 = new App();
         System.out.println("Hello, World!");
     }
 }
