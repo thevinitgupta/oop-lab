719250bc25fdc6976aaf287f4e1de11fc20d1ff4
Vinit Gupta <vinitman35@gmail.com>
2021-05-07 12:39:10 +0530

    add q-6 of lab-3

---
+++ src/Comp.java
@@ -0,0 +1,33 @@
+public class Comp {
+    void compare(int a,int b){
+        int max = a>b ? a : b;
+        System.out.println("Greater value is "+max);
+    }
+    void compare(char a,char b){
+        int aNum = (int) a;
+        int bNum = (int) b;
+        if(aNum>bNum){
+            System.out.println(a+" has greater numeric value!");
+        }
+        else if(bNum>aNum){
+            System.out.println(b+" has greater numeric value!");
+        }
+        else {
+            System.out.println("Both have same numeric value!");
+        }
+    }
+    void compare(String a,String b){
+        if(a.length()>b.length()){
+            System.out.println(a);
+        }
+        else {
+            System.out.println(b);
+        }
+    }
+    public static void main(String[] args) {
+        Comp value = new Comp();
+        value.compare("Vinit", "Shruti");
+        value.compare('v', 'a');
+        value.compare(345, -23);
+    }
+}
