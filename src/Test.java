ea9cb2f446ce51e2b36d2a306db4015c456d1888
Vinit Gupta <vinitman35@gmail.com>
2021-06-14 11:14:39 +0530

    add java documentation link

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,4 @@
-
+//*java documentation -> https://docs.oracle.com/cd/A87860_01/doc/appdev.817/a77069/index.htm
 public class Test {
     public static final void main(String args[]){
         System.out.println("Enter");
