ddb12afbf787ea9bfd1cab5f09f2532f5054cba1
Vinit Gupta <vinitman35@gmail.com>
2021-06-14 12:48:14 +0530

    add solutions to CA-2 from Q14-Q29

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,6 @@
 //*java documentation -> https://docs.oracle.com/cd/A87860_01/doc/appdev.817/a77069/index.htm
 public class Test {
-    public static final void main(String args[]){
-        System.out.println("Enter");
+    public static void main(String args[]){
+       System.out.println(args[2]);
     }
 }
