035833542d72ca02ca1342d956cc22dc7e00b7bf
Vinit Gupta <vinitman35@gmail.com>
2021-06-15 19:40:55 +0530

    update test.java with code implmenting final and static values

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,16 @@
 //*java documentation -> https://docs.oracle.com/cd/A87860_01/doc/appdev.817/a77069/index.htm
 public class Test {
+    static int start = 2;
+    final int end;
+    public Test(int x){
+        x = 4;
+        end = x;
+    }
+    public void fly(int distace){
+        System.out.print(end-start+" ");
+        System.out.println(distace);
+    }
     public static void main(String args[]){
-       System.out.println(args[2]);
+       new Test(10).fly(5);
     }
 }
